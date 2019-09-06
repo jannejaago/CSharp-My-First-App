f09ef1c5386d962934496ee0418dc71687c43f05
Opilane TTHK <opilane@tthk.ee>
2019-09-06 17:03:01 +0300

    Commitment

---
+++ MyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NO HELLO FOR YOU");
+            Console.WriteLine("Sup, how may I call you?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Fuck off "+name);
+     
+            Console.ReadLine();
+        }
+    }
+}
